daa222d6c35cb9e3ec0452b594cb9e6361e6591e
MuhammadIhsanSubiantara <145647059+MuhammadIhsanSubiantara@users.noreply.github.com>
2023-10-19 14:20:15 +0700

    Add files via upload

---
+++ v_header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+          <meta charset="UTF-8">
+          <title>Web Prog II Merancang Template sederhana dengan cedeigniter</title>
+          <link rel="stylesheet" type="text/css" href="http://localhost/pustaka-booking/assets/css/stylebuku.css">
+</head>
+<body>
+          <div id="wrapper">
+                    <header>
+                              <hgroup>
+                                        <h1>Rental.buku.net</h1>
+                                        <h3>Membuat Template sederhana dengan Codeigniter </h3>
+                              </hgroup>
+                    <nav>
+                    <ul>
+                    
+                    <li><a href="<?php echo base_url(). 'web' ?>">Home</a></li>
+                    <li><a href="<?php echo base_url(). 'web/about' ?>">About</a></li>
+                    
+                    </ul>
+                    </nav>
+          <div class="clear"></div>
+</header>
